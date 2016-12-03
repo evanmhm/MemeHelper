1da181843ce9ab65a12ee825c1874d2452b92b72
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2016-12-02 23:25:11 -0800

    Fix mode selection

---
+++ app/assets/js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    $("#emoji").click(function(){
+        console.log('emoji mode');
+    });
+
+    $("#aesthetic").click(function(){
+        console.log('aesthetic mode');
+    });
+
+    $("#random").click(function(){
+        console.log('random mode');
+    });
+});
