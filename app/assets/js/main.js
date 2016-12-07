97ec602303dec32805a1e57015ccaced90747319
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2016-12-06 17:27:21 -0800

    Add clipboard copy

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,24 +1,39 @@
+const {
+    clipboard
+} = require('electron');
+
 var currentMode = "";
 
-$("#emoji").click(function(){
+$("#emoji").click(function() {
     currentMode = "emoji";
     update($("#input-TA").val());
 });
 
-$("#aesthetic").click(function(){
+$("#aesthetic").click(function() {
     currentMode = "aesthetic";
     update($("#input-TA").val());
 });
 
-$("#random").click(function(){
+$("#random").click(function() {
     currentMode = "random";
     update($("#input-TA").val());
 });
 
 $("#input-TA").on("input", function() {
-	update($("#input-TA").val());
+    update($("#input-TA").val());
 });
 
 $("#randomize").click(function() {
     update($("#input-TA").val());
-})
+});
+
+$("#copy").click(function() {
+    content = $("#output-TA").val();
+    console.log($("#output-TA").val());
+    clipboard.writeText(content);
+    if ($('#form').css('opacity') == 0) {
+        $('#form').css('opacity', 1);
+    } else {
+        $('#form').css('opacity', 0);
+    }
+});
